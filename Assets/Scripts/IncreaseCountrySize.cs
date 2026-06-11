17e5c8b29c38b6ca457cde2160a815d0a95e4e56
Lucas Williams <lucasnwilliams2008@gmail.com>
2026-06-11 19:42:12 -0400

    Tiles expand towards mouse click from previous tile

--- Assets/Scripts/IncreaseCountrySize.cs
+++ Assets/Scripts/IncreaseCountrySize.cs
@@ -1,35 +1,93 @@
+using System;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class IncreaseCountrySize : MonoBehaviour
 {
-    public GameObject childGameObject;
-    public GameObject newChildGameObject;
-    private InputAction _expandAction;
+    public GameObject territoryGameObject;
+    public GameObject newTerritoryGameObject;
+    private InputAction _attackAction;
     
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        childGameObject = this.transform.GetChild(0).gameObject;
-        _expandAction = InputSystem.actions.FindAction("Jump");
+        territoryGameObject = this.transform.GetChild(0).gameObject;
+        _attackAction = InputSystem.actions.FindAction("Attack");
         
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(_expandAction.WasPressedThisFrame())
+        if(_attackAction.WasPressedThisFrame())
         {
-            ExpandCountry();
+            Vector3 mousePos = Mouse.current.position.ReadValue();
+            // mousePos.z = 10;
+            // Vector3 mouseWorldPos = Camera.main.ScreenToWorldPoint(mousePos);
+            // Vector3 mouseWorldPos = Mouse.current.position.ReadValue();
+            ExpandCountry(mousePos);
         };
     }
 
-    private void ExpandCountry()
+    private void ExpandCountry(Vector3 targetPosition)
     {
-        newChildGameObject = Instantiate(childGameObject, this.transform);
-        newChildGameObject.gameObject.name = "Plane" + this.transform.childCount;
-        newChildGameObject.transform.position = new Vector3(newChildGameObject.transform.position.x + 10, newChildGameObject.transform.position.y, newChildGameObject.transform.position.z);
-        // newChildGameObject.transform.position = this.transform.position.z + 1;
-        childGameObject = this.transform.GetChild(this.transform.childCount - 1).gameObject;
+
+
+        Ray ray = Camera.main.ScreenPointToRay(targetPosition);
+        
+        Plane horizontalPlane = new Plane(Vector3.up, new Vector3(0, 1, 0));
+
+        // 3. Intersect the ray with the plane
+        if (horizontalPlane.Raycast(ray, out float enter))
+        {
+            // Get the exact Vector3 point where the ray crossed the plane
+            // return ray.GetPoint(enter);
+            targetPosition = ray.GetPoint(enter);
+        }
+        
+        
+        
+        
+        newTerritoryGameObject = Instantiate(territoryGameObject, this.transform);
+        newTerritoryGameObject.gameObject.name = "Territory" + this.transform.childCount;
+        
+        float newXPosition = newTerritoryGameObject.transform.position.x;
+        
+        
+        float newZPosition = newTerritoryGameObject.transform.position.z;
+        
+        float xDistance = targetPosition.x - newTerritoryGameObject.transform.position.x;
+        float zDistance = targetPosition.z - newTerritoryGameObject.transform.position.z;
+        
+        float absXDistance = Mathf.Abs(xDistance);
+        float absZDistance = Mathf.Abs(zDistance);
+
+        if (absXDistance > absZDistance)
+        {
+            if (xDistance > 0)
+            {
+                newXPosition = newTerritoryGameObject.transform.position.x + 10;
+            }
+            else
+            {
+                newXPosition = newTerritoryGameObject.transform.position.x - 10;
+            }
+        } else
+        {
+            if (zDistance > 0)
+            {
+                newZPosition = newTerritoryGameObject.transform.position.z + 10;
+            }
+            else
+            {
+                newZPosition = newTerritoryGameObject.transform.position.z - 10;
+            }
+        }
+        
+        
+
+        newTerritoryGameObject.transform.position = new Vector3(newXPosition, newTerritoryGameObject.transform.position.y, newZPosition);
+        // newTerritoryGameObject.transform.position = new Vector3(newTerritoryGameObject.transform.position.x + 10, newTerritoryGameObject.transform.position.y, newTerritoryGameObject.transform.position.z);
+        territoryGameObject = this.transform.GetChild(this.transform.childCount - 1).gameObject;
     }
 }
